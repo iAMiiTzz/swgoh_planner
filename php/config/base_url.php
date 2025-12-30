c4b2e5b39e3d511ec66205a2618ee0f485dd300c
iAMiiTzz <iAMiiTzz@users.noreply.github.com>
2025-12-30 12:50:31 -0800

    Revert to ?? operator since PHP 8.1 supports it - cleaner code

--- php/config/base_url.php
+++ php/config/base_url.php
@@ -2,7 +2,7 @@
 // Get base URL dynamically
 function getBaseUrl() {
     $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
-    $host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : (isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : 'localhost');
+    $host = $_SERVER['HTTP_HOST'] ?? $_SERVER['SERVER_NAME'] ?? 'localhost';
     
     // Remove port if present
     $host = preg_replace('/:\d+$/', '', $host);
